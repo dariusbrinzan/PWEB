b289d215d70e7d284f31fb38a3c264a23be2f36f
dariusbrinzan <brinzandarius27@gmail.com>
2024-04-24 22:57:50 +0300

    Comments on token

--- Backend/LiverpoolWebsite.BLL/Helper/TokenHelper.cs
+++ Backend/LiverpoolWebsite.BLL/Helper/TokenHelper.cs
@@ -13,6 +13,8 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
+// operatiuni legate de token
+// se foloseste JWT si interactioneaza cu ASP.NET Core
 namespace LiverpoolWebsite.BLL.Helpers
 {
     public class TokenHelper : ITokenHelper
@@ -20,32 +22,38 @@ namespace LiverpoolWebsite.BLL.Helpers
         private readonly UserManager<User> _userManager;
         private readonly IConfiguration _configuration;
 
+        // constructor care init user manager si config
         public TokenHelper(UserManager<User> userManager, IConfiguration configuration)
         {
             _userManager = userManager;
             _configuration = configuration;
         }
-
+        // creez user token pentru utilizator
         public async Task<string> CreateAccessToken(User _User)
-        {
+        {   
+            // preiau ID-urile
             var userId = _User.Id .ToString();
             var userName = _User.UserName;
 
+            // creez lista de claims
             var claims = new List<Claim>
             {
                 new Claim(ClaimTypes.NameIdentifier, userId),
                 new Claim(ClaimTypes.Name, userName)
             };
 
+            // verific rol-ul utilizatorului (user / admin)
             var roles = await _userManager.GetRolesAsync(_User);
 
             foreach (var role in roles) claims.Add(new Claim(ClaimTypes.Role, role));
 
+            // preiau cheia secreta si cheia simetrica
             var secret = _configuration.GetSection("Jwt").GetSection("Token").Get<String>();
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
 
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
+            // token descriptor
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(claims),
@@ -54,14 +62,15 @@ namespace LiverpoolWebsite.BLL.Helpers
             };
 
             var tokenHandler = new JwtSecurityTokenHandler();
-
+            // creez token
             var token = tokenHandler.CreateToken(tokenDescriptor);
 
             return tokenHandler.WriteToken(token);
         }
-
+        // refresh token
         public string CreateRefreshToken()
         {
+            // numar random pentru refresh token
             var randomNumber = new byte[32];
             using (var rng = RandomNumberGenerator.Create())
             {
@@ -70,6 +79,7 @@ namespace LiverpoolWebsite.BLL.Helpers
             }
         }
 
+        // preia principalul dintr-un token care a expirat
         public ClaimsPrincipal GetPrincipalFromExpiredToken(string _Token)
         {
             var tokenValidationParameters = new TokenValidationParameters
@@ -85,6 +95,7 @@ namespace LiverpoolWebsite.BLL.Helpers
             var tokenHandler = new JwtSecurityTokenHandler();
             SecurityToken securityToken;
 
+            // valideaza token si returneaza principal
             var principal = tokenHandler.ValidateToken(_Token, tokenValidationParameters, out securityToken);
             var jwtSecurityToken = securityToken as JwtSecurityToken;
             if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals("hs512", StringComparison.InvariantCultureIgnoreCase))
